5e8933e9a9f580f453674168ec06b3fe6b25d20a
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-10-12 13:12:57 -0700

    Add object var queryResult

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 
-
-var movie;
-var imdbID;
-var title;
-var runtime;
-var rated;
-var imdbScore;
-var rottenScore;
-var metaScore;
-var userScore;
-var director;
-var cast;
-var nytSnippet;
-var nytAuthor
-var services;
-var summary;
-var genre;
+// don't need -- putting all these into object variable queryResult
+// var movie;
+// var imdbID;
+// var title;
+// var runtime;
+// var rated;
+// var imdbScore;
+// var rottenScore;
+// var metaScore;
+// var tmdbScore;
+// var director;
+// var cast;
+// var nytSnippet;
+// var nytAuthor
+// var services;
+// var summary;
+// var genre;
 
 var wait=0;
 var popularClicked=0;
@@ -49,40 +49,54 @@ popularButton.on("click",function(){
     fetchPopular();
 })
 
+// queryResult is used to gather the current search result data from our set of queries
+var queryResult = {
+  cast: "",
+  director: "",
+  genre: ""
+  rating: "",
+  review: { nyt: { author: "", snippet: "" } },
+  runtime: 0,
+  score: { imdb: 0, rotten: 0, tmdb: 0 },
+  streaming: "",
+  summary: "",
+  title: "",
+};
 
 
-  function appendCard(){
-    // console.log("works");
-    // movieCards.text("");
-    var movieCard=$("<section>").addClass("movie-card");
-    var scoresEl=$("<h3>").text("Scores:");
-    titleEl=$("<h2>").text(title);
-    runtimeEl=$("<p>").text("Runtime: "+ runtime+" minutes");
-    ratedEl=$("<p>").text("Rated: "+rated);
-    imdbScoreEl=$("<p>").text("IMDB: "+ imdbScore);
-    rottenScoreEl=$("<p>").text("Rotten Tomatoes: "+rottenScore);
-    metaScoreEl=$("<p>").text("Meta Critic: "+metaScore);
-    userScoreEl=$("<p>").text("User Score: "+userScore);
-    directorEl=$("<p>").text("Director: "+ director);
-    castEl=$("<p>").text("Cast: "+cast);
-    nytSnippetEl=$("<p>").text("Review: "+nytSnippet);
-    nytAuthorEl=$("<p>").text("Author: "+nytAuthor);
-    servicesEl=$("<p>").text("services: " +services);
-    summaryEl=$("<p>").text(summary);
-    genreEl=$("<p>").text("Genre: "+ genre);
-    titleEl.append(runtimeEl,ratedEl,genreEl,directorEl,castEl,summaryEl);
-    scoresEl.append(imdbScoreEl,rottenScoreEl,metaScoreEl,userScoreEl);
-    nytSnippetEl.append(nytAuthorEl);
-    movieCard.append(titleEl,scoresEl,nytSnippetEl);
-    movieCards.prepend(movieCard);
-    
-    if (popularClicked&&count<popularArr.length){
-        count++
-    fetchOMDB(popularArr[count])
-    }
-    // console.log("movie card")
-    // console.log(movieCards);
+
+function appendCard(){
+  // console.log("works");
+  // movieCards.text("");
+  var movieCard=$("<section>").addClass("movie-card");
+  var scoresEl=$("<h3>").text("Scores:");
+  titleEl=$("<h2>").text(title);
+  runtimeEl=$("<p>").text("Runtime: "+ runtime+" minutes");
+  ratedEl=$("<p>").text("Rated: "+rated);
+  imdbScoreEl=$("<p>").text("IMDB: "+ imdbScore);
+  rottenScoreEl=$("<p>").text("Rotten Tomatoes: "+rottenScore);
+  metaScoreEl=$("<p>").text("Meta Critic: "+metaScore);
+  tmdbScoreEl=$("<p>").text("User Score: "+tmdbScore);
+  directorEl=$("<p>").text("Director: "+ director);
+  castEl=$("<p>").text("Cast: "+cast);
+  nytSnippetEl=$("<p>").text("Review: "+nytSnippet);
+  nytAuthorEl=$("<p>").text("Author: "+nytAuthor);
+  servicesEl=$("<p>").text("services: " +services);
+  summaryEl=$("<p>").text(summary);
+  genreEl=$("<p>").text("Genre: "+ genre);
+  titleEl.append(runtimeEl,ratedEl,genreEl,directorEl,castEl,summaryEl);
+  scoresEl.append(imdbScoreEl,rottenScoreEl,metaScoreEl,tmdbScoreEl);
+  nytSnippetEl.append(nytAuthorEl);
+  movieCard.append(titleEl,scoresEl,nytSnippetEl);
+  movieCards.prepend(movieCard);
+  
+  if (popularClicked&&count<popularArr.length){
+      count++
+  fetchOMDB(popularArr[count])
   }
+  // console.log("movie card")
+  // console.log(movieCards);
+}
 
 
 function fetchPopular(){
@@ -132,10 +146,10 @@ const options = {
     //   console.log(response);
         return response.json().then(function(data){
         console.log("TMDB specific movie")
-        title=data.original_title;
-        userScore=data.vote_average;
-        runtime=data.runtime;
-        summary=data.overview;
+        queryResult.title = data.original_title;
+        queryResult.score.tmdb = data.vote_average;
+        queryResult.runtime = data.runtime;
+        queryResult.summary = data.overview;
         // console.log(data);
         // console.log(data.original_title);
         // console.log(data.vote_average);
@@ -188,51 +202,51 @@ fetch(omdbUrl)
 }
 
 function fetchNYTReview(movie){
-//fetch to nyt review of movie
-reviewUrl="https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name%3A%22Movies%22%20AND%20type_of_material%3A%22Review%22&q="+movie+"&api-key=pf1jPMp9J2Gq6kH3AyhwAUUl2zEIlDBm";
-// fetch(reviewUrl)
-// .then(function(response){
-//     if (response.ok){
-//         //   console.log(response);
-//             return response.json().then(function(data){
-//             console.log("nyt review")
-//             // console.log(data);
-//             // console.log(data.response);
-//             // console.log(data.response.docs[0]);
-//             // console.log(data.response.docs[0].lead_paragraph);
-//             // console.log(data.response.docs[0].snippet);
-//             nytSnippet=data.response.docs[0].snippet;
-//             nytAuthor=data.response.docs[0].byline.original;
-//             fetchServices(movie);
-//             })
-//         }
-//     })
-fetchServices(movie);
+  //fetch to nyt review of movie
+  reviewUrl="https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=section_name%3A%22Movies%22%20AND%20type_of_material%3A%22Review%22&q="+movie+"&api-key=pf1jPMp9J2Gq6kH3AyhwAUUl2zEIlDBm";
+  // fetch(reviewUrl)
+  // .then(function(response){
+  //     if (response.ok){
+  //         //   console.log(response);
+  //             return response.json().then(function(data){
+  //             console.log("nyt review")
+  //             // console.log(data);
+  //             // console.log(data.response);
+  //             // console.log(data.response.docs[0]);
+  //             // console.log(data.response.docs[0].lead_paragraph);
+  //             // console.log(data.response.docs[0].snippet);
+  //             nytSnippet=data.response.docs[0].snippet;
+  //             nytAuthor=data.response.docs[0].byline.original;
+  //             fetchServices(movie);
+  //             })
+  //         }
+  //     })
+  fetchServices(movie);
 }
 
 function fetchServices(movie){
-// sees if movie is streaming based on imdb id
-const urlStreaming = 'https://streaming-availability.p.rapidapi.com/search/title?title='+movie+'&country=us&show_type=movie&output_language=en';
-const options1={
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '325e143123msh6e7bf9effd3a82dp190444jsn2a9f8b54b6f6',
-		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
-	}
-};
-console.log("services section");
-//commenting out to reduce number of calls
-// fetch(urlStreaming,options1)
-//     .then(function(response){
-//         if (response.ok){
-//     //   console.log(response);
-//         return response.json().then(function(data){
-//         // console.log("streaming service")
-//         // console.log(data.result)
-//         // console.log(data.result[0].streamingInfo)
-//         services=data.result[0].streamingInfo.us[0].service;
-//         })
-//     }
-// })
-appendCard();
+  // sees if movie is streaming based on imdb id
+  const urlStreaming = 'https://streaming-availability.p.rapidapi.com/search/title?title='+movie+'&country=us&show_type=movie&output_language=en';
+  const options1={
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '325e143123msh6e7bf9effd3a82dp190444jsn2a9f8b54b6f6',
+      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+  };
+  console.log("services section");
+  //commenting out to reduce number of calls
+  // fetch(urlStreaming,options1)
+  //     .then(function(response){
+  //         if (response.ok){
+  //     //   console.log(response);
+  //         return response.json().then(function(data){
+  //         // console.log("streaming service")
+  //         // console.log(data.result)
+  //         // console.log(data.result[0].streamingInfo)
+  //         services=data.result[0].streamingInfo.us[0].service;
+  //         })
+  //     }
+  // })
+  appendCard();
 }
